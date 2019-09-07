5788871666ecc67a0cca1efe1dce4fa278e4e2d5
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-07 16:18:32 -0700

    connects multiple users to a socket

--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@ let s = new server({port: 5000});
 
 s.on('connection', function(ws) {
   ws.on('message', function(message) {
-    console.log('Received ', message);
+    message = JSON.parse(message);
+
+    if(message.type === 'name') {
+      ws.personName = message.data;
+    }
 
     s.clients.forEach(function e(client) {
       if(client !== ws)
-      client.send(message);
+        client.send(JSON.stringify({
+          name: ws.personName,
+          data: message.data
+        }));
     });
     
     // ws.send('From server: ' + message);
