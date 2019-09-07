26f817c531898ea1fd2f90a918e1dbfae7b60ba4
macandrew6 <mr.betterandrewmac@gmail.com>
2019-09-06 18:12:23 -0700

    completes basic chat function

--- server/index.js
+++ server/index.js
@@ -4,7 +4,17 @@ let s = new server({port: 5000});
 s.on('connection', function(ws) {
   ws.on('message', function(message) {
     console.log('Received ', message);
-    ws.send('From server: ' + message);
+
+    s.clients.forEach(function e(client) {
+      if(client !== ws)
+      client.send(message);
+    });
+    
+    // ws.send('From server: ' + message);
+  });
+
+  ws.on('close', function() {
+    console.log('i lost a client');
   });
 
   console.log('one more client connected')
